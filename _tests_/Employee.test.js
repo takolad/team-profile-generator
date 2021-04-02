d5b8d4dc3576e3d5b22e0ecf734a88f494d3b1da
John Hinojosa <hinojohn@ymail.com>
2021-04-02 12:23:09 -0500

    added validate.js to validate user entered information, additional work on employee testing

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -9,8 +9,42 @@ describe('Employee class', () => {
             
             const emp = new Employee(empName, empId, empEmail);
 
-            expect(emp.name).toEqual(empName);
-            expect(emp.id).toEqual(empId);
-            expect(emp.email).toEqual(empEmail);
+            expect(emp).toEqual( { 
+                name : empName, 
+                id : empId, 
+                email : empEmail } );
+    })
+
+    it("should throw an error when no name is entered", () => {
+        const empName = "";
+        const empId = 0;
+        const empEmail = "janed@fakemail.com";
+
+        const emp = () => new Employee(empName, empId, empEmail);
+        const err = new Error ("A name must be entered.");
+
+        expect(emp).toThrowError(err);
+    })
+    
+    it("should throw an error when a negative id is entered", () => {
+        const empName = "Rick Donles";
+        const empId = -8;
+        const empEmail = "rickd@fakemail.com";
+
+        const emp = () => new Employee(empName, empId, empEmail);
+        const err = new Error ("A non-negative id number is required.");
+
+        expect(emp).toThrowError(err);
+    })
+
+    it("should throw an error when an invalid email is entered", () => {
+        const empName = "Zack Unfaire";
+        const empId = 1;
+        const empEmail = "zackrulz.com";
+
+        const emp = () => new Employee(empName, empId, empEmail);
+        const err = new Error ("Invalid email format entered.")
+
+        expect(emp).toThrowError(err);
     })
 })
